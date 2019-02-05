460dcb0c3b1a1e2f9d7225f7fa9253ac2fac9742
Yaki <igor.yakunin@quadream.io>
2019-02-05 14:33:26 +0200

    fix "latest" tag and image name. (conflicted with old nodejs project)

--- py/recipes/ui/recipe.py
+++ py/recipes/ui/recipe.py
@@ -5,10 +5,19 @@ from server.deployment import kube, Recipe
 class QuantumUI(Recipe):
     def load_components(self):
         super(QuantumUI, self).load_components()
-        
+
+        d = kube.Deployment(
+            name='quantum-ui',
+            image='sr-rnd-105.swg.local:5000/go-ng-ui-quantum:{}'.format(
+                os.environ.get('GO_SERVER_CORE_IMAGE_TAG', 'latest')),
+            ports=[
+                {'containerPort': 80},
+                ],
+        )
+
         self.add_components([
-            kube.File(os.path.join(self.dir_path, 'k8s/quantumui-deployment.yaml')),
-            kube.File(os.path.join(self.dir_path, 'k8s/quantumui-service.yaml'))
+            d,
+            kube.File(os.path.join(self.dir_path, 'k8s/quantumui-service.yaml')),
         ])
 
 class ReignUI(Recipe):
