da3ed6aeb600428d15e8379ee0439ad3d544eb6c
Guy Geva <guyg@quadream.io>
2019-03-05 18:01:53 +0200

    fix

--- py/recipes/ui/recipe.py
+++ py/recipes/ui/recipe.py
@@ -5,64 +5,70 @@ from server.deployment import kube, Recipe
 
 
 class QuantumUI(Recipe):
-  def _add_ingress(self):
-    rules = [
-      {
-        'host': os.environ['K8S_INGRESS_QUANTUM_UI'],
-        'http': {
-          'paths': [
+    def _add_ingress(self):
+        rules = [
             {
-              'path': '/',
-              'backend': {
-                'serviceName': 'quantum-ui',
-                'servicePort': 80
-              }
+                'host': os.environ['K8S_INGRESS_QUANTUM_UI'],
+                'http': {
+                    'paths': [
+                        {
+                            'path': '/',
+                            'backend': {
+                                'serviceName': 'quantum-ui',
+                                'servicePort': 80
+                            }
+                        }
+                    ]
+                }
             }
-          ]
-        }
-      }
-    ]
-    i = kube.Ingress(name='quantum-ui-ingress', rules=rules)
-    self.add_component(i)
+        ]
+        i = kube.Ingress(name='quantum-ui-ingress', rules=rules)
+        self.add_component(i)
 
-  def load_components(self):
-    super(QuantumUI, self).load_components()
+    def load_components(self):
+        super(QuantumUI, self).load_components()
 
-    d = kube.Deployment(
-      name='quantum-ui',
-      image='sr-rnd-105.swg.local:5000/go-client-quantum-ui:{}'.format(
-        os.environ.get('GO_SERVER_QUANTUM_UI_IMAGE_TAG', 'latest')),
-      image_pull_policy='IfNotPresent',
-      ports=[
-        {'containerPort': 80},
-      ],
-      volume_mounts=[
-        {
-          'name': 'quantum-ui-config',
-          'mountPath': '/usr/share/nginx/html/assets/config/appConfig.json',
-          'subPath': 'appConfig.json',
-        }
-      ],
-      volumes=[
-        {
-          'name': 'quantum-ui-config',
-          'configMap': {
-            'name': 'quantum-ui-config'
-          }
-        }
-      ]
-    )
+        d = kube.Deployment(
+            name='quantum-ui',
+            image='sr-rnd-105.swg.local:5000/go-client-quantum-ui:{}'.format(
+                os.environ.get('GO_SERVER_QUANTUM_UI_IMAGE_TAG', 'latest')),
+            image_pull_policy='IfNotPresent',
+            ports=[
+                {'containerPort': 80},
+            ],
+            volume_mounts=[
+                {
+                    'name': 'quantum-ui-config',
+                    'mountPath': '/usr/share/nginx/html/assets/config/appConfig.json',
+                    'subPath': 'appConfig.json',
+                }
+            ],
+            volumes=[
+                {
+                    'name': 'quantum-ui-config',
+                    'configMap': {
+                        'name': 'quantum-ui-config'
+                    }
+                }
+            ]
+        )
 
-    self.add_component(d)
-    self.add_component(kube.File(os.path.join(self.dir_path, 'k8s/quantumui-service.yaml')))
+        s = kube.Service(
+            name='quantum-ui',
+            service_port=80,
+            target_port=8080,
+        )
 
-    if is_env_enabled('K8S_INGRESS'):
-      if is_env_enabled('K8S_LOCAL_DEV'):
-        self.add_component(kube.File(os.path.join(self.dir_path, 'k8s/quantumui-ingress-dev-config.yaml')))
-      else:
-        self.add_component(kube.File(os.path.join(self.dir_path, 'k8s/quantumui-ingress-prod-config.yaml')))
-    else:
-      self.add_component(kube.File(os.path.join(self.dir_path, 'k8s/quantumui-default-config.yaml')))
+        self.add_component(d)
+        self.add_component(s)
 
-    self._add_ingress()
+        if is_env_enabled('K8S_LOCAL_DEV'):
+            self.add_component(kube.File(os.path.join(
+                self.dir_path,
+                'k8s/quantumui-ingress-dev-config.yaml')))
+        else:
+            self.add_component(kube.File(os.path.join(
+                self.dir_path,
+                'k8s/quantumui-ingress-prod-config.yaml')))
 
+        self._add_ingress()
