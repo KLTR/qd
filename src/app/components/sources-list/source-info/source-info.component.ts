ba025eb9216cba67c56d65fe6c338883e4114117
Roy <roy.levy@quadream.io>
2019-03-17 13:16:13 +0200

    added contacts intels, styles

--- src/app/components/sources-list/source-info/source-info.component.ts
+++ src/app/components/sources-list/source-info/source-info.component.ts
@@ -16,6 +16,29 @@ contacts = [];
 
   ngOnInit() {
     this.http.getSourcesTasks(this.source.id).subscribe(res => {console.log(res),this.tasks = res.tasks})
+    this.contacts = [
+      {  
+        name : 'Roy Levy',
+        mobile : '0544753336' ,
+        email : 'Hotmailsuck@gmail.com'
+      },
+      {   
+        name : 'Tomer Pomodoro',
+        mobile : '0544753312' ,
+        email : 'gmailsucks@hotmail.com'
+    },
+    {   
+      name : 'Zlatan Ibrahimovic',
+      mobile : '0541753312' ,
+      email : 'Shark@fish.com',
+      profileImg: 'https://www.thewistle.com/wp-content/uploads/2017/10/Zlatan-Ibrahimovic.jpg'
+  },
+  {   
+    name : 'Theirry Henry',
+    mobile : '0241753312' ,
+    email : 'Scorer@gunner.com'
+}
+    ]
   }
 
 
