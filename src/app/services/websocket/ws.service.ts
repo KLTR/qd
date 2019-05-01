410f9d755abcbff3d0bb794e0428cc5240276b85
Roy <roy.levy@quadream.io>
2019-05-01 11:56:12 +0300

    contacts

--- src/app/services/websocket/ws.service.ts
+++ src/app/services/websocket/ws.service.ts
@@ -1,21 +1,13 @@
-
-import {
-  Injectable
-} from '@angular/core';
+import { Injectable } from '@angular/core';
 import * as Rx from 'rxjs';
-import {
-  map,
-  share,
-  retry,
-} from 'rxjs/operators';
-import { webSocket } from 'rxjs/webSocket';
+import { map, retry, share } from 'rxjs/operators';
 import { AppConfigService } from '../app-config/app-config.service';
 @Injectable()
 export class WsService {
-  public messages: Rx.Subject < string > ;
+  public messages: Rx.Subject<string>;
   public ws: any;
   public isConnected = false;
-  private subject: Rx.Subject < MessageEvent > ;
+  private subject: Rx.Subject<MessageEvent>;
   private token: string;
   private env: any;
 
@@ -23,81 +15,48 @@ export class WsService {
     this.env = this.appConfig.getConfig();
   }
 
-  public open(){
+  public open() {
     this.token = localStorage.getItem('token');
-    if(this.token){
-      this.messages = < Rx.Subject < any >>
-      this.connect(this.env.webSocketUrl)
-      .pipe(
-        map((response: MessageEvent): any => {
-          let data = JSON.parse(response.data);
-          return data;
-        })
-      )
-      console.log("Websocket successfully connected to : ", this.env.webSocketUrl);
+    if (this.token) {
+      this.messages = <Rx.Subject<any>>this.connect(this.env.webSocketUrl).pipe(
+        map(
+          (response: MessageEvent): any => {
+            const data = JSON.parse(response.data);
+            return data;
+          }
+        )
+      );
     }
+    console.log('Websocket successfully connected to : ', this.env.webSocketUrl);
   }
 
-
-  public connect(url): Rx.Subject < MessageEvent > {
+  public connect(url): Rx.Subject<MessageEvent> {
     if (!this.subject) {
       this.subject = this.create(url);
     }
     return this.subject;
   }
-  private create(url): Rx.Subject < MessageEvent > {
-
-    this.ws = new WebSocket(
-      url,
-      [this.env.wsProtocol, this.token]
-    );
-    const observable = Rx.Observable.create(
-      (obs: Rx.Observer < MessageEvent > ) => {
-        this.ws.onmessage = obs.next.bind(obs);
-        this.ws.onerror = obs.error.bind(obs);
-        this.ws.onclose = obs.complete.bind(obs);
-        return this.ws.close.bind(this.ws);
-      }
-    ).pipe(
+  private create(url): Rx.Subject<MessageEvent> {
+    this.ws = new WebSocket(url, [this.env.wsProtocol, this.token]);
+    const observable = Rx.Observable.create((obs: Rx.Observer<MessageEvent>) => {
+      this.ws.onmessage = obs.next.bind(obs);
+      this.ws.onerror = obs.error.bind(obs);
+      this.ws.onclose = obs.complete.bind(obs);
+      return this.ws.close.bind(this.ws);
+    }).pipe(
       share(),
       retry()
-    )
+    );
     const observer = {
       next: (data: Object) => {
         if (this.ws.readyState === WebSocket.OPEN) {
           this.ws.send(JSON.stringify(data));
         }
       }
-    }
+    };
     return Rx.Subject.create(observer, observable);
   }
 
-
-  // private create(url): Rx.Subject < MessageEvent > {
-
-  //   this.ws = new WebSocket(
-  //     url,
-  //     [this.env.wsProtocol, this.token]
-  //   );
-  //   const observable = Rx.Observable.create(
-  //     (obs: Rx.Observer < MessageEvent > ) => {
-  //       this.ws.onmessage = obs.next.bind(obs);
-  //       this.ws.onerror = obs.error.bind(obs);
-  //       this.ws.onclose = obs.complete.bind(obs);
-  //       return this.ws.close.bind(this.ws);
-  //     }
-  //   ).pipe(
-  //     share()
-  //   )
-  //   const observer = {
-  //     next: (data: Object) => {
-  //       if (this.ws.readyState === WebSocket.OPEN) {
-  //         this.ws.send(JSON.stringify(data));
-  //       }
-  //     }
-  //   }
-  //   return Rx.Subject.create(observer, observable);
-  // }
   public close() {
     if (this.ws) {
       this.ws.close();
